6f5b5cfa9bccfbabe17c075cecee10b914715efe
loi-go-1220 <loi.go1220@gmail.com>
2024-12-19 12:58:44 -0600

    Update: Profile Page

--- app/profile/index.tsx
+++ app/profile/index.tsx
@@ -1,11 +1,11 @@
 import { StyleSheet, Pressable, Image } from "react-native";
 import { Link, router } from "expo-router";
 import { Text, View } from "@/components/Themed";
-import { Ionicons } from '@expo/vector-icons';
+import { Ionicons } from "@expo/vector-icons";
 import { useAuth } from "@/context/AuthContext";
+import Divider from "@/components/Divider";
 
 export default function ProfileScreen() {
-  
   const { signOut } = useAuth();
   const handleLogout = async () => {
     // Add your logout logic here, for example:
@@ -21,7 +21,7 @@ export default function ProfileScreen() {
         <Text style={styles.name}>James Stewart</Text>
         <Text style={styles.department}>IT</Text>
       </View>
-
+      <Divider />
       <View style={styles.section}>
         <Text style={styles.sectionTitle}>Contact details</Text>
         <View style={styles.contactItem}>
@@ -34,6 +34,7 @@ export default function ProfileScreen() {
         </View>
       </View>
 
+      <Divider />
       <View style={styles.section}>
         <Text style={styles.sectionTitle}>Saved credit card</Text>
         <Pressable style={styles.addCardButton}>
@@ -42,6 +43,7 @@ export default function ProfileScreen() {
         </Pressable>
       </View>
 
+      <Divider />
       <View style={styles.section}>
         <Text style={styles.sectionTitle}>Default concurrency</Text>
         <Pressable style={styles.currencySelector}>
@@ -56,6 +58,7 @@ export default function ProfileScreen() {
         </Pressable>
       </View>
 
+      <Divider />
       <View style={styles.footer}>
         <Link href="/auth" style={styles.forgotPassword}>
           Forgot password?
@@ -87,36 +90,37 @@ export default function ProfileScreen() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: 'white',
-    paddingHorizontal: 20,
+    backgroundColor: "white",
   },
   header: {
-    fontSize: 16,
-    fontWeight: '600',
-    textAlign: 'center',
-    marginTop: 16,
+    fontSize: 20,
+    fontWeight: "600",
+    textAlign: "center",
+    marginTop: 32,
     marginBottom: 24,
   },
   section: {
-    marginBottom: 24,
+    marginVertical: 20,
+    paddingHorizontal: 20,
   },
   sectionTitle: {
-    fontSize: 14,
-    color: '#64748B',
+    fontSize: 17,
+    color: "#1e1e1e",
+    fontWeight: "700",
     marginBottom: 12,
   },
   name: {
-    fontSize: 16,
-    fontWeight: '500',
+    fontSize: 15,
+    fontWeight: "500",
   },
   department: {
-    fontSize: 14,
-    color: '#64748B',
-    marginTop: 4,
+    fontSize: 15,
+    color: "#64748B",
+    marginTop: 12,
   },
   contactItem: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
     marginBottom: 12,
   },
   contactText: {
@@ -124,30 +128,32 @@ const styles = StyleSheet.create({
     fontSize: 14,
   },
   addCardButton: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "center",
     borderWidth: 1,
-    borderColor: '#E2E8F0',
+    borderColor: "#E2E8F0",
     borderRadius: 8,
     padding: 12,
   },
   addCardText: {
     marginLeft: 8,
-    color: '#1E3A8A',
-    fontSize: 14,
+    color: "#1e1e1e",
+    fontWeight: "700",
+    fontSize: 17,
   },
   currencySelector: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    justifyContent: 'space-between',
+    flexDirection: "row",
+    alignItems: "center",
+    justifyContent: "space-between",
     borderWidth: 1,
-    borderColor: '#E2E8F0',
+    borderColor: "#E2E8F0",
     borderRadius: 8,
     padding: 12,
   },
   currencyOption: {
-    flexDirection: 'row',
-    alignItems: 'center',
+    flexDirection: "row",
+    alignItems: "center",
   },
   flag: {
     width: 24,
@@ -159,46 +165,52 @@ const styles = StyleSheet.create({
   },
   footer: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
+    marginTop: 20,
   },
   forgotPassword: {
-    color: '#64748B',
+    color: "#64748B",
     marginBottom: 16,
+    textDecorationLine: "underline",
+    fontSize: 17,
   },
   logout: {
-    color: '#EF4444',
+    color: "#EF4444",
+    textDecorationLine: "underline",
+    marginTop: 16,
+    fontSize: 17,
   },
   tabBar: {
-    flexDirection: 'row',
-    justifyContent: 'space-around',
-    alignItems: 'center',
+    flexDirection: "row",
+    justifyContent: "space-around",
+    alignItems: "center",
     borderTopWidth: 1,
-    borderTopColor: '#E2E8F0',
+    borderTopColor: "#E2E8F0",
     paddingVertical: 8,
-    backgroundColor: 'white',
-    position: 'absolute',
+    backgroundColor: "white",
+    position: "absolute",
     bottom: 0,
     left: 0,
     right: 0,
   },
   tabItem: {
-    alignItems: 'center',
+    alignItems: "center",
   },
   addButton: {
-    backgroundColor: '#1E3A8A',
+    backgroundColor: "#1E3A8A",
     width: 48,
     height: 48,
     borderRadius: 24,
-    alignItems: 'center',
-    justifyContent: 'center',
+    alignItems: "center",
+    justifyContent: "center",
     marginBottom: 20,
   },
   tabText: {
     fontSize: 12,
-    color: '#64748B',
+    color: "#64748B",
     marginTop: 4,
   },
   activeTab: {
-    color: '#1E3A8A',
+    color: "#1E3A8A",
   },
 });
