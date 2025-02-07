84813ee95fae741969127e996808533edc35a1f5
Sung-Hyun Kang <sunghyunkang@microsoft.com>
2025-02-06 21:39:22 -0600

    added icons

--- metro.config.js
+++ metro.config.js
@@ -1,6 +1,8 @@
 const { getDefaultConfig } = require("expo/metro-config");
 const { withNativeWind } = require('nativewind/metro');
 
-const config = getDefaultConfig(__dirname)
+const config = getDefaultConfig(__dirname);
 
-module.exports = withNativeWind(config, { input: './global.css' })
+config.resolver.assetExts.push("ttf");
+
+module.exports = withNativeWind(config, { input: './global.css' });
